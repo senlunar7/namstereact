59cf4130d9f553c53740bbf9c990c20cacbf0e11
Chander Sen <115090022+senlunar7@users.noreply.github.com>
2023-02-04 16:35:55 +0530

    Add files via upload

---
+++ namstereact/App.js
@@ -0,0 +1,17 @@
+// Manipulated the HTML DOM using Javscript
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Namaste Everyone";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+// Manipulate the HTML DOM using React
+
+// Create nested React Elements
+const heading1 = React.createElement("h1", { id: "title1" }, "Heading 1");
+const heading2 = React.createElement("h2", { id: "title2" }, "Heading 2");
+const containerX = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(containerX);
